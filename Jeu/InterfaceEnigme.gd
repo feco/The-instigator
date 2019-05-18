0190a4eecc06fb47c275bd17e76ffe773d5eeb9c
Yoaime <feco.veil@gmail.com>
2019-05-18 10:21:46 +0200

    Avancement jusqu'au choix de l'instigateur

--- Jeu/InterfaceEnigme.gd
+++ Jeu/InterfaceEnigme.gd
@@ -1,8 +1,36 @@
 extends MarginContainer
 
-func _preparer(enigme):
-	$"DécoupageInterfaceEn2/Enigme/Question".text = enigme["question"]
-	for reponse in enigme["choix"]:
-		var bouton_reponse = Button.new()
-		bouton_reponse.text = reponse
-		$"DécoupageInterfaceEn2/Enigme/Défilement/Réponses".add_child(bouton_reponse)
+var reponses = []
+
+#Fonction qui sert à afficher le contenu de l'énigme
+func _preparer(enigme, choix_joueurs = null):
+	print("Préparation de l'énigme")
+	print(enigme)
+	if $"DécoupageInterfaceEn2/Enigme/Défilement/Réponses".get_child_count() == 0:
+		$"DécoupageInterfaceEn2/Enigme/Question".text = enigme["question"]
+		if choix_joueurs != null:
+			$"DécoupageInterfaceEn2/Enigme/Votes".text = choix_joueurs
+		for reponse in enigme["choix"]:
+			var bouton_reponse = Button.new()
+			bouton_reponse.text = reponse
+			var police = DynamicFont.new()
+			police.font_data = load("res://arial.ttf")
+			police.size = 30
+			bouton_reponse.add_font_override("font", police)
+			bouton_reponse.connect("pressed", self, "_on_reponse", [bouton_reponse])
+			reponses.append(bouton_reponse);
+			$"DécoupageInterfaceEn2/Enigme/Défilement/Réponses".add_child(bouton_reponse)
+
+#Fonction qui sert à réinitialiser l'interface énigme
+func _vider():
+	print("Vider énigme")
+	$"DécoupageInterfaceEn2/Enigme/Votes".text = ""
+	for child in $"DécoupageInterfaceEn2/Enigme/Défilement/Réponses".get_children():
+		child.queue_free()
+
+#Fonction qui envoie le choix du joueur au serveur
+func _on_reponse(bouton):
+	print("Le joueur envoie sa réponse")
+	_vider()
+	get_parent()._on_reponse(bouton.text)
+	hide()
--- Jeu/Jeu.gd
+++ Jeu/Jeu.gd
@@ -2,29 +2,57 @@ extends Spatial
 
 var enigmes = null
 var enigme_actuelle
+var reponses_joueurs = []
+var enigme_en_cours = false
+var enigme_finie = false
+var instigateur
 
 #Initialisation du jeu
 func _ready():
+	print(globals.liste_des_joueurs)
+	print("Mon id : ")
+	print(get_tree().get_network_unique_id())
 	randomize() #Initialise le système de génération de nombre aléatoire
 	_charger_enigmes()
 	if globals.est_serveur :
 		_choix_instigateur()
-		for i in range(10):
-			_poser_enigme()
+		_poser_enigme()
+
+
+func _process(delta):
+	if globals.est_serveur:
+		if enigme_en_cours:
+			if reponses_joueurs.size() >= globals.liste_des_joueurs.size():
+				enigme_en_cours = false
+				rpc("_proposer_options_instigateur", reponses_joueurs);
+
+
 
 #Choisir un instigateur au hasard
 func _choix_instigateur():
 	var instigateur_number = randi() % globals.liste_des_joueurs.size()
-	var instigateur = globals.liste_des_joueurs[instigateur_number]
+	instigateur = globals.liste_des_joueurs[instigateur_number]
 	#Envoie l'infos à l'instigateur sélectionné qu'il est l'instigateur
 	print("Instigateur : ")
 	print(instigateur["id"])
-	rpc_id(instigateur["id"], "_est_instigateur")
+	if instigateur["id"] != 0 :
+		rpc_id(instigateur["id"], "_est_instigateur")
+	else :
+		_est_instigateur()
 
 #Fonction qui élève le joueur courant au niveau d'instigateur si le serveur lui en envoie l'ordre
 remote func _est_instigateur():
 	print("Vous êtes un instigateur")
 	globals.est_instigateur = true
+
+#Fonction qui trouve le nom d'un joueur en fonction de son id
+func _recuperer_nom_par_id(id):
+	for joueur in globals.liste_des_joueurs:
+		if joueur["id"] == id:
+			return joueur["pseudo"]
+
+
+
 
 
 
@@ -69,10 +97,11 @@ func _charger_enigmes():
 #Fonction qui lance le déroulement d'une énigme
 func _poser_enigme():
 	var enigme = null
+	var reponses_joueurs = []
 	_selectionner_enigme()
 	_afficher_enigme(enigme)
-	#_attendre_reponses()
-	#_proposer_options_instigateur()
+	enigme_en_cours = true
+	#A partir de ce moment on attend que tout le monde a répondu, c'est la fonction _process qui gère ça
 
 #Fonction qui choisi une énigme au hasard
 func _selectionner_enigme():
@@ -83,10 +112,64 @@ func _afficher_enigme(enigme):
 	#Envoie l'ordre par le réseau à tout le monde d'afficher l'énigme
 	rpc("_joueurs_afficher_enigme", enigme_actuelle)
 
+#Fonction qui réceptionne les réponses des joueurs à une énigme ou réponse de l'instigateur
+remote func _reponse_joueur(reponse, id_appelant):
+	print("Réception de la réponse du joueur")
+	if globals.est_serveur:
+		print("Le récepteur est bien le serveur")
+		if enigme_en_cours:
+			print("Une énigme est en cours, traitement de la réponse...")
+			for reponse in reponses_joueurs:
+				if reponse["id_joueur"] == id_appelant:
+					return
+			
+			reponses_joueurs.append({"id_joueur" : id_appelant, "reponse" : reponse})
+		elif id_appelant == instigateur["id"]:
+			print("Traitement du choix de l'instigateur")
+			traiter_choix_instigateur(reponse)
+
+#Fonction qui gère le choix de l'instigateur après une énigme
+func traiter_choix_instigateur(reponse):
+	pass
+
+
+
+
+
 
 # --------- ENIGME côté client ---------
 
+#Fonction qui affiche l'énigme sélectionnée par le serveur aux clients
 sync func _joueurs_afficher_enigme(enigme_numero):
 	enigme_actuelle = enigme_numero
 	$InterfaceEnigme._preparer(enigmes[enigme_actuelle])
 	$InterfaceEnigme.show()
+
+#Fonction qui affiche à l'instigateur ses choix après une énigme
+sync func _proposer_options_instigateur(reponses_joueurs):
+	print("Afficher options pour l'instigateur")
+	if globals.est_instigateur:
+		var choix_joueurs = "Votes : "
+		for reponse in reponses_joueurs :
+			choix_joueurs += _recuperer_nom_par_id(reponse["id_joueur"]) + " a voté " + reponse["reponse"] + ";"
+		var enigme = {"question": "En tant qu'instigateur que souhaitez-vous faire ?", "choix":["1", "2", "3", "4"]}
+		print("Enigme to prepare :")
+		print(enigme)
+		$InterfaceEnigme._preparer(enigme, choix_joueurs)
+		$InterfaceEnigme.show()
+
+#Fonction qui traite la réponse donnée par un joueur
+func _on_reponse(reponse):
+	if (globals.est_serveur):
+		if enigme_en_cours:
+			print("Une énigme est en cours, traitement de la réponse...")
+			for reponse in reponses_joueurs:
+				if reponse["id_joueur"] == 0:
+					return
+			
+			reponses_joueurs.append({"id_joueur" : 0, "reponse" : reponse})
+		elif 0 == instigateur["id"]:
+			print("Traitement du choix de l'instigateur")
+			traiter_choix_instigateur(reponse)
+	else:
+		rpc("_reponse_joueur", reponse, get_tree().get_network_unique_id())
